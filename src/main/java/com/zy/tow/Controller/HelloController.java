2c83f089e6052674eef546864e66f31b61f28880
Unknown <2416677458@qq.com>
2020-04-09 15:32:46 +0800

    代码保存

--- src/main/java/com/zy/tow/Controller/HelloController.java
+++ src/main/java/com/zy/tow/Controller/HelloController.java
@@ -2,6 +2,8 @@ package com.zy.tow.Controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping
 public class HelloController {
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
     @RequestMapping("hello")
     @ResponseBody
     public String say(){
